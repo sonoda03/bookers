45f8fcd5e3c313b163c4c9214a467f2cb2d91db7
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-11 09:32:34 +0000

    [Add]routes変更(resources :books追加&変更,topのurlを「/」に変更)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get '/top' => 'homes#top'
+  get '/' => 'homes#top'
+
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  
+  resources :books
 end
