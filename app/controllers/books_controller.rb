3edb4a0c3975d94c66fc00304d3b0fdc67d43e78
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-12 05:47:59 +0000

    [Add]新規登録機能,create機能,indexページ,showページ,editページ,update機能,destroy機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
@@ -14,9 +17,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to 
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy  
+    redirect_to  books_path
   end
   
   private
